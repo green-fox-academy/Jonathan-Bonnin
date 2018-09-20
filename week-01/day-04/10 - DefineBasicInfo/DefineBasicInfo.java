88efab2c36ca6279c7056e49aa8ea4ab23a395cc
Jonathan Bonnin <bonninj@gmail.com>
2018-09-20 11:04:18 +0200

    add java algorithm 10 - DefineBasicInfo

---
+++ week-01/day-04/10 - DefineBasicInfo/DefineBasicInfo.java
@@ -0,0 +1,24 @@
+//#TODO
+//public class DefineBasicInfo {
+//    public static void main(String[] args) {
+//        // Define several things as a variable then print their values
+//        // Your name as a string
+//        // Your age as an integer
+//        // Your height in meters as a double
+//        // Whether you are married or not as a boolean
+//    }
+//}
+
+public class DefineBasicInfo {
+
+    public static void main(String[] args) {
+        String name = new String("Jonathan");
+        int age = 28;
+        double height = 1.87;
+        boolean isMarried = false;
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Married? " + isMarried);
+    }
+}
