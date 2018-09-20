06dc46b0aab6e3269058784c06a114e0fac2c47e
Jonathan Bonnin <bonninj@gmail.com>
2018-09-20 09:26:39 +0200

    add java algorithm 01 - HelloMe

---
+++ week-01/day-04/HelloMe.java
@@ -0,0 +1,13 @@
+// #INSTRUCTIONS:
+// public class HelloMe {
+//    public static void main(String[] args) {
+//        // Modify this program to greet you instead of the World!
+//        System.out.println("Hello, World!");
+//    }
+//}
+
+public class HelloMe {
+    public static void main (String[] args) {
+        System.out.println("Hello Jonathan");
+    }
+}
