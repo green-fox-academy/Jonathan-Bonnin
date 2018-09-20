42b0c3994c23791fea465bd3f98754be5954fa91
Jonathan Bonnin <bonninj@gmail.com>
2018-09-20 10:57:10 +0200

    add java algorithm 09 - BMI

---
+++ week-01/day-04/09 - BMI/BMI.java
@@ -0,0 +1,20 @@
+//#TODO
+//public class Bmi {
+//    public static void main(String[] args) {
+//        double massInKg = 81.2;
+//        double heightInM = 1.78;
+//
+//        // Print the Body mass index (BMI) based on these values
+//    }
+//}
+
+public class BMI {
+
+    public static void main(String[] args) {
+        double massInKg = 81.0;
+        double heightInM = 1.87;
+        System.out.println("The BMI is: " + massInKg/(heightInM*heightInM));
+    }
+}
+
+
