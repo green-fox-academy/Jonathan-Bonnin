01e0cb6a9e597e515417a57a923fca138134b005
Jonathan Bonnin <bonninj@gmail.com>
2018-09-20 17:34:29 +0200

    add java algorithm 32 - DrawDiagonal

---
+++ week-01/day-04/32 - DrawDiagonal/DrawDiagonal.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class DrawDiagonal {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int sideLength = input.nextInt();
+        if (sideLength > 0) {
+            for (int i = 1; i <= sideLength; i++) {
+                String drawing = "";
+                if (i == 1 || i == sideLength){
+                    for (int j = 0; j < sideLength; j++) {
+                        drawing += "%";
+                    }
+                    System.out.println(drawing);
+                } else {
+                    for (int j = 1; j <= sideLength; j++) {
+                        if (j == 1 || j == sideLength || i == j) {
+                            drawing += "%";
+                        } else {
+                            drawing += " ";
+                        }
+                    }
+                    System.out.println(drawing);
+                }
+            }
+
+        } else {
+            System.out.println("Please give me an integer superior to 0");
+        }
+    }
+}
