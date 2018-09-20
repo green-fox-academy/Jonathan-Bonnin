c803cdc11f47794f10872dff6c5254f31fe90fc6
Jonathan Bonnin <bonninj@gmail.com>
2018-09-20 10:38:27 +0200

    add java algorithm 06 - CodingHours

---
+++ week-01/day-04/06 - CodingHours/CodingHours.java
@@ -0,0 +1,21 @@
+//#TODO
+//public class CodingHours {
+//    public static void main(String[] args) {
+//        // An average Green Fox attendee codes 6 hours daily
+//        // The semester is 17 weeks long
+//        //
+//        // Print how many hours is spent with coding in a semester by an attendee,
+//        // if the attendee only codes on workdays.
+//        //
+//        // Print the percentage of the coding hours in the semester if the average
+//        // work hours weekly is 52
+//    }
+//}
+
+public class CodingHours {
+
+    public static void main(String[] args) {
+        System.out.println("An average Green Fox attendee will code " + 17*5*6 + " hours throughout the duration of the boot camp");
+        System.out.println("Assuming a 52h work week, a student codes about " + 100*30/52 + "% of the time");
+    }
+}
