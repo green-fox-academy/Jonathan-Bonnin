f9938b6e04fda0d97eb47e61c548116e895d6bce
Jonathan Bonnin <bonninj@gmail.com>
2018-09-20 09:41:57 +0200

    add java algorithm 03 - HelloOthers

---
+++ week-01/day-04/03 - HelloOthers/HelloOthers.java
@@ -0,0 +1,18 @@
+//#INSTRUCTIONS
+// public class HelloOthers {
+//    public static void main(String[] args) {
+//        // Greet 3 of your classmates with this program, in three separate lines
+//        // like:
+//        //
+//        // Hello, Esther!
+//        // Hello, Mary!
+//        // Hello, Joe!
+//    }
+//}
+
+
+public class HelloOthers {
+    public static void main (String[] args) {
+        System.out.println("Hello, Daniel!\nHello, Sam!\nHello, Patrick!");
+    }
+}
