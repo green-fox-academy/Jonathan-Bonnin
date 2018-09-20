7fa5f6a4154ac8eb79d01db8b5cf35018ebad019
Jonathan Bonnin <bonninj@gmail.com>
2018-09-20 10:44:32 +0200

    add java algorithm 07 - FavoriteNumber

---
+++ week-01/day-04/07 - FavoriteNumber/FavoriteNumber.java
@@ -0,0 +1,16 @@
+//#TODO
+//public class FavoriteNumber {
+//    public static void main(String[] args) {
+//        int favoriteNumber = 8;
+//        // Store your favorite number in a variable (as a number)
+//        // And print it like this: "My favorite number is: 8"
+//    }
+//}
+
+public class FavoriteNumber {
+
+    public static void main(String[] args) {
+	int favoriteNumber = 2;
+        System.out.println("My favorite number is: " + favoriteNumber);
+    }
+}
