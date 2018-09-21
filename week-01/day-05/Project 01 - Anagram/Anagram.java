f6ef4e0d748ea35115d7829e7355a8bef0c9205a
Jonathan Bonnin <bonninj@gmail.com>
2018-09-21 11:47:36 +0200

    add java algorithm project 01 - Anagram

---
+++ week-01/day-05/Project 01 - Anagram/Anagram.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Anagram {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Gimme 2 strings, I'll tell you if they're an anagram");
+        String str1 = input.nextLine();
+        String str2 = input.nextLine();
+        System.out.println(isAnagran(str1, str2));
+    }
+
+    public static boolean isAnagran(String str1, String str2){
+        // removes all non letters from the 2 strings
+        // creates a lower case only string
+        str1 = str1.replaceAll("[^a-zA-Z]", "").toLowerCase();
+        str2 = str2.replaceAll("[^a-zA-Z]", "").toLowerCase();
+
+        if (str1.length() != str2.length()){
+            return false;
+        } else {
+            for (int i = 0; i < str1.length(); i++) {
+                if (str2.indexOf(str1.charAt(i)) == -1 ){
+                    return false;
+                }
+            }
+
+        }
+        return true;
+
+    }
+}
