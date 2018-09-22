cc6a68e13ceb45a68ff60a3ef567bc380bd355d6
Jonathan Bonnin <bonninj@gmail.com>
2018-09-22 15:13:56 +0200

    changed Palindrome Searcher to add output in array format

--- week-01/day-05/Project 03 - Palindrome searcher/PalindromeChecker.java
+++ week-01/day-05/Project 03 - Palindrome searcher/PalindromeChecker.java
@@ -7,15 +7,46 @@ public class PalindromeChecker {
         System.out.println("Yo dog, gimme a string and I'll give you all of its palindromes");
         Scanner input = new Scanner(System.in);
         String str = input.nextLine();
-        //System.out.println(Arrays.toString(palindromeSearcher(stringToCheck)));
+        int palindromeCounter = 0;
+
+        //looping through every possible sub string to count palindromes
+
         for (int i = 0; i < str.length(); i++) {
             for (int j = i+1; j <=  str.length(); j++) {
                 String subStr = str.substring(i, j);
                 if (subStr.length() > 2 && isPalindrome(subStr) == true) {
-                    System.out.println(subStr);
+                    palindromeCounter++;
                 }
             }
         }
+
+        System.out.println("Our hard working team found " + palindromeCounter + " palindromes...");
+
+        //creates an array with as many elements as palindromes
+
+        String[] arrayOfPalindromes = new String[palindromeCounter];
+        int arrayIndex = 0;
+
+        //re-loops through every possible sub string to add palindromes to array
+
+        for (int i = 0; i < str.length(); i++) {
+            for (int j = i+1; j <=  str.length(); j++) {
+                String subStr = str.substring(i, j);
+                if (subStr.length() > 2 && isPalindrome(subStr) == true) {
+
+                    //the first palindrome found is added at index 0 using the value arrayIndex
+
+                    arrayOfPalindromes[arrayIndex] = "\"" + subStr + "\"";
+
+                    //increments arrayIndex in order to add the next palindrome at the next index
+
+                    arrayIndex++;
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(arrayOfPalindromes));
+
     }
 
     public static boolean isPalindrome(String str){
