f68a3b5f2b6a87694de987bee5ab89b9ff36c218
Jonathan Bonnin <bonninj@gmail.com>
2018-09-24 17:02:27 +0200

    add java algorithm 04 - Todo Print

---
+++ week-02/day-02/04 - Todo Print/Main.java
@@ -0,0 +1,31 @@
+//public class TodoPrint {
+//    public static void main(String... args){
+//        String todoText = " - Buy milk\n";
+//        // Add "My todo:" to the beginning of the todoText
+//        // Add " - Download games" to the end of the todoText
+//        // Add " - Diablo" to the end of the todoText but with indention
+//
+//        // Expected outpt:
+//
+//        // My todo:
+//        //  - Buy milk
+//        //  - Download games
+//        //      - Diablo
+//
+//        System.out.println(todoText);
+//    }
+//}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String todoText = "- Buy milk \n";
+        todoText = "My todo:\n" + todoText;
+        todoText += "- Download games\n";
+        todoText += "\t - Diablo";
+
+
+        System.out.println(todoText);
+    }
+}
