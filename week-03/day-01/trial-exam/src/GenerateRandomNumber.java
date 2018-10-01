04fb275a92c1959deaab1e0ce6736bcae0915a33
Jonathan Bonnin <bonninj@gmail.com>
2018-10-01 12:48:35 +0200

    add exam-trial question time

---
+++ week-03/day-01/trial-exam/src/GenerateRandomNumber.java
@@ -0,0 +1,7 @@
+public class GenerateRandomNumber {
+    public static void main(String[] args) {
+
+        //this generates and prints a random integer between 0 and 99 included:
+        System.out.println((int) (Math.random()*100));
+    }
+}
