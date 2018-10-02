c7948a58041cc2212aa49fb19d68571dbe69f27b
Jonathan Bonnin <bonninj@gmail.com>
2018-10-02 11:29:14 +0200

    add exercise divide by zero

--- week-03/day-01/trial-exam/src/GenerateRandomNumber.java
+++
@@ -1,7 +0,0 @@
-public class GenerateRandomNumber {
-    public static void main(String[] args) {
-
-        //this generates and prints a random integer between 0 and 99 included:
-        System.out.println((int) (Math.random()*100));
-    }
-}
---
+++ week-03/day-02/ExceptionExercises/src/DivideByZero.java
@@ -0,0 +1,21 @@
+// Create a function that takes a number
+// divides ten with it,
+// and prints the result.
+// It should print "fail" if the parameter is 0
+
+public class DivideByZero {
+
+    public static void main(String[] args) {
+        
+        try {
+            divideTenBy(0);
+        } catch (Exception e) {
+            System.out.println("fail");
+        }
+
+    }
+
+    public static int divideTenBy(int num) throws Exception {
+        return 10 / num;
+    }
+}
