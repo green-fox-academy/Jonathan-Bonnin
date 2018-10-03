a944f719f823db7dd494a0461d9741f092638f25
Jonathan Bonnin <bonninj@gmail.com>
2018-10-03 12:20:15 +0200

    add exercise Pokemon

--- week-03/day-03/src/Week03Day03.java
+++ week-03/day-03/src/Week03Day03.java
@@ -1,17 +1,46 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Week03Day03 {
 
-//    Create a PostIt class that has
-//            a backgroundColor
-//            a text on it
-//            a textColor
-//    Create a few example post-it objects:
-//    an orange with blue text: "Idea 1"
-//    a pink with black text: "Awesome"
-//    a yellow with green text: "Superb!"
+    public static void main(String[] args) {
+        List<Pokemon> pokemonOfAsh = initializePokemons();
 
-    PostIt postIt1 = new PostIt("orange", "Idea 1", "blue");
-    PostIt postIt2 = new PostIt("pink", "Awesome", "black");
-    PostIt postIt3 = new PostIt("yellow", "Superb!", "green");
+        // Every pokemon has a name and a type.
+        // Certain types are effective against others, e.g. water is effective against fire.
+
+        // Ash has a few pokemon.
+        // A wild pokemon appeared!
+
+        Pokemon wildPokemon = new Pokemon("Oddish", "leaf", "water");
+
+        // Which pokemon should Ash use?
+
+        String effectivePokemon = "";
+
+        for (Pokemon pokemon : pokemonOfAsh) {
+            if(pokemon.isEffectiveAgainst(wildPokemon)){
+                effectivePokemon = pokemon.name;
+                break;
+            }
+        }
+
+        System.out.print("I choose you, " + effectivePokemon);
+    }
+
+    private static List<Pokemon> initializePokemons() {
+        List<Pokemon> pokemon = new ArrayList<>();
+
+        pokemon.add(new Pokemon("Balbasaur", "leaf", "water"));
+        pokemon.add(new Pokemon("Pikatchu", "electric", "water"));
+        pokemon.add(new Pokemon("Charizard", "fire", "leaf"));
+        pokemon.add(new Pokemon("Balbasaur", "water", "fire"));
+        pokemon.add(new Pokemon("Kingler", "water", "fire"));
+
+        return pokemon;
+    }
+
+
 
 //    Create a BlogPost class that has
 //            an authorName
@@ -30,7 +59,16 @@ public class Week03Day03 {
     BlogPost blog2 = new BlogPost("Wait but why", "Tim Urban", "2010.10.10.", "A popular long-form, stick-figure-illustrated blog about almost everything.");
     BlogPost blog3 = new BlogPost("One Engineer Is Trying to Get IBM to Reckon With Trump", "William Turton", "2017.03.28.", "Daniel Hanley, a cybersecurity engineer at IBM, doesn’t want to be the center of attention. When I asked to take his picture outside one of IBM’s New York City offices, he told me that he wasn’t really into the whole organizer profile thing.");
 
+//    Create a PostIt class that has
+//            a backgroundColor
+//            a text on it
+//            a textColor
+//    Create a few example post-it objects:
+//    an orange with blue text: "Idea 1"
+//    a pink with black text: "Awesome"
+//    a yellow with green text: "Superb!"
 
-
-
+    PostIt postIt1 = new PostIt("orange", "Idea 1", "blue");
+    PostIt postIt2 = new PostIt("pink", "Awesome", "black");
+    PostIt postIt3 = new PostIt("yellow", "Superb!", "green");
 }
