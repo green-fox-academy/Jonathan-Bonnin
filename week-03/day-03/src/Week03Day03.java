58b3beff5ac2235a4bd8a067cb6314363467e1a2
Jonathan Bonnin <bonninj@gmail.com>
2018-10-03 11:56:07 +0200

    add exercise PostIt

---
+++ week-03/day-03/src/Week03Day03.java
@@ -0,0 +1,15 @@
+public class Week03Day03 {
+
+//    Create a PostIt class that has
+//            a backgroundColor
+//            a text on it
+//            a textColor
+//    Create a few example post-it objects:
+//    an orange with blue text: "Idea 1"
+//    a pink with black text: "Awesome"
+//    a yellow with green text: "Superb!"
+
+    PostIt postIt1 = new PostIt("orange", "Idea 1", "blue");
+    PostIt postIt2 = new PostIt("pink", "Awesome", "black");
+    PostIt postIt3 = new PostIt("yellow", "Superb!", "green");
+}
