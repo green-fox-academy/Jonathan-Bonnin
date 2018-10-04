44bab26a9f8d85d973bbe29f507d013b48c3fdf0
Jonathan Bonnin <bonninj@gmail.com>
2018-10-04 10:44:29 +0200

    add exercise Number Adder

--- week-03/day-04/src/Counter.java
+++ week-03/day-04/src/Counter.java
@@ -2,14 +2,14 @@ public class Counter {
 
     public static void main(String[] args) {
 
-    // Write a recursive function that takes one parameter: n and counts down from n.
+        // Write a recursive function that takes one parameter: n and counts down from n.
         counter(10);
     }
 
-    static int counter(int n){
-        if (n > 0){
+    static int counter(int n) {
+        if (n > 0) {
             System.out.println(n);
-            return counter(n-1);
+            return counter(n - 1);
         }
         System.out.println(n);
         return n;
