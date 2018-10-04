78275cd388b26ef56a1a5b3fdcc6c58ab838dd8d
Jonathan Bonnin <bonninj@gmail.com>
2018-10-04 13:16:45 +0200

    changed YellowSquare to return void

--- week-03/day-04/src/YellowSquares.java
+++ week-03/day-04/src/YellowSquares.java
@@ -11,22 +11,26 @@ public class YellowSquares {
         graphics.setColor(Color.yellow);
         graphics.fillRect(0, 0, 729, 729);
         graphics.setColor(Color.black);
-        drawFourSquares(WIDTH / 3., WIDTH/2, HEIGHT/2, graphics);
+        drawFourSquares(WIDTH / 3., WIDTH / 2, HEIGHT / 2, graphics);
     }
 
-    static int drawFourSquares(double n, double xCenter, double yCenter, Graphics graphics) {
+    static void drawFourSquares(double n, double xCenter, double yCenter, Graphics graphics) {
         if (n < 3) {
-            return 0;
+            return;
         }
 
-        graphics.drawLine((int) (xCenter - n/2), (int) (yCenter - n * 1.5), (int) (xCenter - n/2), (int) (yCenter + n * 1.5));
-        graphics.drawLine((int) (xCenter + n/2), (int) (yCenter - n * 1.5), (int) (xCenter + n/2), (int) (yCenter + n * 1.5));
+        graphics.drawLine((int) (xCenter - n / 2), (int) (yCenter - n * 1.5), (int) (xCenter - n / 2), (int) (yCenter + n * 1.5));
+        graphics.drawLine((int) (xCenter + n / 2), (int) (yCenter - n * 1.5), (int) (xCenter + n / 2), (int) (yCenter + n * 1.5));
         graphics.drawLine((int) (xCenter - n * 1.5), (int) (yCenter - n / 2), (int) (xCenter + n * 1.5), (int) (yCenter - n / 2));
         graphics.drawLine((int) (xCenter - n * 1.5), (int) (yCenter + n / 2), (int) (xCenter + n * 1.5), (int) (yCenter + n / 2));
 
 
-        return drawFourSquares(n/3, xCenter, yCenter-n, graphics) + drawFourSquares(n/3, xCenter, yCenter+n, graphics)
-        + drawFourSquares(n/3, xCenter-n, yCenter, graphics) + drawFourSquares(n/3, xCenter+n, yCenter, graphics);
+        drawFourSquares(n / 3, xCenter, yCenter - n, graphics);
+        drawFourSquares(n / 3, xCenter, yCenter + n, graphics);
+        drawFourSquares(n / 3, xCenter - n, yCenter, graphics);
+        drawFourSquares(n / 3, xCenter + n, yCenter, graphics);
+
+        return;
 
     }
 
