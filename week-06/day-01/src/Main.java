df232a974007b0504b6cb29357e533b9ec04b40b
Jonathan Bonnin <bonninj@gmail.com>
2018-10-29 12:36:47 +0100

    add stream exercise 3

--- week-06/day-01/src/Main.java
+++ week-06/day-01/src/Main.java
@@ -5,6 +5,15 @@ import java.util.OptionalDouble;
 public class Main {
 
     public static void main(String[] args) {
+        //Exercise 3
+        //Write a Stream Expression to get the squared value of the positive numbers from the following array:
+        ArrayList<Integer> numbers3 = new ArrayList<>(Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14));
+        numbers3.stream().
+                filter(n -> n >= 0).
+                map(n -> n * n).
+                forEach(System.out::println);
+        System.out.println();
+
         //Exercise 2
         //Write a Stream Expression to get the average value of the odd numbers from the following array:
         ArrayList<Integer> numbers2 = new ArrayList<>(Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14));
@@ -13,6 +22,7 @@ public class Main {
                 mapToDouble(n -> n).
                 average();
         System.out.println(average.getAsDouble());
+        System.out.println();
 
 	    // Exercise 1;
         //Write a Stream Expression to get the even numbers from the following array:
