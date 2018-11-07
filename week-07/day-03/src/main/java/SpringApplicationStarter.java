efb8970ade362969ae339a0834c09010fafc5170
Jonathan Bonnin <bonninj@gmail.com>
2018-11-07 13:24:40 +0100

    add exercise hello DI

---
+++ week-07/day-03/src/main/java/SpringApplicationStarter.java
@@ -0,0 +1,26 @@
+package com.gfa.w7d3;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.LocalDateTime;
+
+
+@SpringBootApplication
+public class SpringApplicationStarter implements CommandLineRunner {
+
+    @Autowired
+    Printer printer;
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringApplicationStarter.class, args);
+    }
+
+
+    @Override
+    public void run(String...args) throws Exception {
+        printer.log("Daniel rimes with miel");
+    }
+}
