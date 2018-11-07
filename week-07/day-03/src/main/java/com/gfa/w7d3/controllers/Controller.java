dbd12e91d7586660c16c5cebdad9c3b6d3760584
Jonathan Bonnin <bonninj@gmail.com>
2018-11-07 15:08:45 +0100

    add exercise e-mail validator

--- week-07/day-03/src/main/java/com/gfa/w7d3/controllers/Controller.java
+++ week-07/day-03/src/main/java/com/gfa/w7d3/controllers/Controller.java
@@ -1,9 +1,15 @@
 package com.gfa.w7d3.controllers;
 
 
+import com.gfa.w7d3.UtilityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @org.springframework.stereotype.Controller
+
+
 
 
 public class Controller {
@@ -12,4 +18,23 @@ public class Controller {
     public String useful(){
         return "useful";
     }
+
+    @Autowired
+    UtilityService utilityService;
+
+    @GetMapping(value = "/useful/colored")
+    public String coloredBackgroud(Model model){
+        model.addAttribute("color", utilityService.randomColor());
+        return "colored-background";
+    }
+
+    @GetMapping(value = "/useful/email")
+    public String email(Model model, @RequestParam String email){
+        boolean validEmail = utilityService.isThisEmailValid(email);
+        model.addAttribute("email", email);
+        model.addAttribute("validEmail", validEmail);
+        return "email";
+    }
+
+
 }
