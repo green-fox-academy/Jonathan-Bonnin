d6ad4829e671ed1a51b6eae03129a3e3c4ba8679
Jonathan Bonnin <bonninj@gmail.com>
2018-11-08 12:27:28 +0100

    initilize project with index.html

---
+++ week-07/day-04/src/main/java/SpringApplicationStarter.java
@@ -0,0 +1,13 @@
+package com.greenfoxacademy.jonathan.tamagotchiapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApplicationStarter {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringApplicationStarter.class, args);
+    }
+}
