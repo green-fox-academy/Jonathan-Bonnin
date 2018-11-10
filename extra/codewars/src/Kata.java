0d4bbb8138ed56683b775e83750e273e2ec9439a
Jonathan Bonnin <bonninj@gmail.com>
2018-11-10 19:04:19 +0100

    add kata create phone number

--- extra/codewars/src/Kata.java
+++ extra/codewars/src/Kata.java
@@ -4,17 +4,34 @@ public final class Kata {
 
     }
 
+    public static String createPhoneNumberSolution(int[] numbers) {
+        return String.format("(%d%d%d) %d%d%d-%d%d%d%d", numbers[0], numbers[1], numbers[2], numbers[3], numbers[4],
+                numbers[5], numbers[6], numbers[7], numbers[8], numbers[9]);
+    }
+
+    public static String createPhoneNumber(int[] numbers) {
+        String phoneNumber = "(";
+        for (int i = 0; i < numbers.length; i++) {
+            phoneNumber += numbers[i];
+            if (i == 2) {
+                phoneNumber += ") ";
+            } else if (i == 5) {
+                phoneNumber += "-";
+            }
+        }
+        return phoneNumber;
+    }
+
     public static int breakChocolate(int n, int m) {
         return n + m > 1 ? n * m - 1 : 0;
     }
 
+    public static String disemvowelSolution(String str) {
+        return str.replaceAll("(?i)[aeiou]", "");
+    }
 
     public static String disemvowel(String str) {
         return str.replaceAll("[aeiouAEIOU]", "");
-    }
-
-    public static String disemvowelSolution(String str) {
-        return str.replaceAll("(?i)[aeiou]", "");
     }
 
     public static int dontGiveMeFive(int start, int end) {
--- extra/codewars/src/Main.java
+++ extra/codewars/src/Main.java
@@ -1,15 +1,25 @@
 public class Main {
 
     public static void main(String[] args) {
-        //https://www.codewars.com/kata/534ea96ebb17181947000ada/train/java
-        Kata.breakChocolate(4, 10);
+        //https://www.codewars.com/kata/create-phone-number/train/java
+        int[] tenNums = {1, 0, 4, 7, 2, 5, 8, 3, 6, 9};
+        System.out.println("\nCreate phone number;");
+        System.out.println(Kata.createPhoneNumber(tenNums));
+        System.out.println(Kata.createPhoneNumberSolution(tenNums));
+
+
+        //https://www.codewars.com/kata/534ea96ebb17181947000ada/train/
+        System.out.println("\nBreak chocolate;");
+        System.out.println(Kata.breakChocolate(4, 10));
 
         //https://www.codewars.com/kata/disemvowel-trolls/train/java
-        Kata.disemvowel("Michel");
-        Kata.disemvowelSolution("Hello, World!");
+        System.out.println("\nRemove vowels;");
+        System.out.println(Kata.disemvowel("Michel"));
+        System.out.println(Kata.disemvowelSolution("Hello, World!"));
 
         //https://www.codewars.com/kata/dont-give-me-five/train/java
-	    Kata.dontGiveMeFive(4, 12);
-	    Kata.dontGiveMeFiveSolution(4, 12);
+        System.out.println("\nDon't give me five;");
+        System.out.println(Kata.dontGiveMeFive(4, 12));
+        System.out.println(Kata.dontGiveMeFiveSolution(4, 12));
     }
 }
