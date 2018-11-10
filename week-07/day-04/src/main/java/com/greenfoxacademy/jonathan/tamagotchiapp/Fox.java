a5eeb983f8e628c4db7558578c47f738253a8221
Jonathan Bonnin <bonninj@gmail.com>
2018-11-10 11:00:30 +0100

    change README.md

--- week-07/day-04/src/main/java/com/greenfoxacademy/jonathan/tamagotchiapp/Fox.java
+++ week-07/day-04/src/main/java/com/greenfoxacademy/jonathan/tamagotchiapp/Fox.java
@@ -1,11 +1,17 @@
 package com.greenfoxacademy.jonathan.tamagotchiapp;
-
+import java.util.ArrayList;
 import java.util.List;
+
 
 public class Fox {
     String name;
-    List<String> tricks;
+    List<String> tricks = new ArrayList<>();
     String food;
+    String drink;
+
+    public Fox(String name) {
+        this.name = name;
+    }
 
     public String getName() {
         return name;
@@ -23,9 +29,17 @@ public class Fox {
         return drink;
     }
 
-    String drink;
-
-    public Fox(String name) {
-        this.name = name;
+    public void setTricks(List<String> tricks) {
+        this.tricks = tricks;
     }
+
+    public void setFood(String food) {
+        this.food = food;
+    }
+
+    public void setDrink(String drink) {
+        this.drink = drink;
+    }
+
+
 }
--- week-07/day-04/src/main/java/com/greenfoxacademy/jonathan/tamagotchiapp/controllers/Controllers.java
+++ week-07/day-04/src/main/java/com/greenfoxacademy/jonathan/tamagotchiapp/controllers/Controllers.java
@@ -1,18 +1,39 @@
 package com.greenfoxacademy.jonathan.tamagotchiapp.controllers;
 
+import com.greenfoxacademy.jonathan.tamagotchiapp.Fox;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Controller
 public class Controllers {
 
-    @GetMapping(value = "")
-    public String home(Model model, @RequestParam String name){
-        model.addAttribute("name", name);
+    List<Fox> foxes;
+
+    public Controllers(){
+        foxes = new ArrayList<>();
+        Fox patrik = new Fox("Patrik");
+        patrik.setFood("Burger");
+        foxes.add(patrik);
+        foxes.add(new Fox("Michel"));
+        foxes.add(new Fox("Jacques"));
+    }
+
+
+    @GetMapping(value = "home/{name}/show")
+    public String home(Model model, @PathVariable String name){
+        Fox myFox = foxes.stream()
+                .filter(f -> f.getName().equals(name))
+                .findFirst()
+                .get();
+        model.addAttribute("fox", myFox);
         return "index";
     }
 
