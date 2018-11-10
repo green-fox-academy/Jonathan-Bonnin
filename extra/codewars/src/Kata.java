5a3b48fd75a0aff56f9572b2ee0f742fa3e53361
Jonathan Bonnin <bonninj@gmail.com>
2018-11-10 16:43:41 +0100

    add kata break chocolate

--- extra/codewars/src/Kata.java
+++ extra/codewars/src/Kata.java
@@ -1,32 +1,37 @@
 public final class Kata {
 
-    private Kata(){
+    private Kata() {
 
     }
 
+    public static int breakChocolate(int n, int m) {
+        return n + m > 1 ? n * m - 1 : 0;
+    }
+
+
     public static String disemvowel(String str) {
-        return str.replaceAll("[aeiouAEIOU]","");
+        return str.replaceAll("[aeiouAEIOU]", "");
     }
 
     public static String disemvowelSolution(String str) {
-        return str.replaceAll("(?i)[aeiou]","");
+        return str.replaceAll("(?i)[aeiou]", "");
     }
 
-    public static int dontGiveMeFive(int start, int end){
+    public static int dontGiveMeFive(int start, int end) {
         int result = 0;
-        for (int i = start; i <= end; i++){
+        for (int i = start; i <= end; i++) {
             String str = Integer.toString(i);
-            if (!str.contains("5")){
+            if (!str.contains("5")) {
                 result++;
             }
         }
         return result;
     }
 
-    public static int dontGiveMeFiveSolution(int start, int end){
+    public static int dontGiveMeFiveSolution(int start, int end) {
         int result = 0;
-        for (int i = start; i <= end; i++){
-            if (!(""+i).contains("5")){
+        for (int i = start; i <= end; i++) {
+            if (!("" + i).contains("5")) {
                 result++;
             }
         }
--- extra/codewars/src/Main.java
+++ extra/codewars/src/Main.java
@@ -1,9 +1,12 @@
 public class Main {
 
     public static void main(String[] args) {
+        //https://www.codewars.com/kata/534ea96ebb17181947000ada/train/java
+        Kata.breakChocolate(4, 10);
+
         //https://www.codewars.com/kata/disemvowel-trolls/train/java
         Kata.disemvowel("Michel");
-        Kata.disemvowel("Hello, World!");
+        Kata.disemvowelSolution("Hello, World!");
 
         //https://www.codewars.com/kata/dont-give-me-five/train/java
 	    Kata.dontGiveMeFive(4, 12);
