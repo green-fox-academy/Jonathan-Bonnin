2c35a66d97c7d13504b3f2f7a8c61aeeca0ac2be
Jonathan Bonnin <bonninj@gmail.com>
2018-11-18 19:13:46 +0100

    add exercise front end

---
+++ week-09/day-01/src/main/java/controller/AppControllers.java
@@ -0,0 +1,11 @@
+package controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class AppControllers {
+
+    @GetMapping("/")
+    public String showIndex(){
+        return "index";
+    }
+}
