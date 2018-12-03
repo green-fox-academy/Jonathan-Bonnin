72ea1693dfb3d46a79021b521b467b1432430084
Jonathan Bonnin <bonninj@gmail.com>
2018-12-03 17:00:07 +0100

    add exercise: are you sleepy?

--- extra/C#/pluralsight-courses/C# Fundamentals with Visual Studio 2015/Hello/Hello/Program.cs
+++ extra/C#/pluralsight-courses/C# Fundamentals with Visual Studio 2015/Hello/Hello/Program.cs
@@ -6,7 +6,21 @@ namespace Hello
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hellooo, " + args[0]);
+            Console.WriteLine("Your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("How much did you sleep last night?");
+            int hoursOfSleep = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Hello, " + name +"!");
+            if(hoursOfSleep >= 8)
+            {
+                Console.WriteLine("You are well rested");
+            }
+            else
+            {
+                Console.WriteLine("You need more sleep");
+            }
+            
         }
     }
 }
